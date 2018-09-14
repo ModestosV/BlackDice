ca589de57806f7150d2c83418e32c808e857fa23
Mathieu Leblanc <0mat.leb2@gmail.com>
2018-09-14 13:55:03 -0400

    Import Kryzarel's Character Stats asset as the base stat and stat modifier system. #6

---
+++ Online Grid Arena/Assets/Scripts/StatModifier.cs
@@ -0,0 +1,36 @@
+/*
+ * Credit: Kryzarel's free Unity asset titled "Character Stats".
+ * Obtained from Unity Asset Store on 2018/09/14. https://assetstore.unity.com/packages/tools/integration/character-stats-106351
+ */
+
+namespace Kryz.CharacterStats
+{
+	public enum StatModType
+	{
+		Flat = 100,
+		PercentAdd = 200,
+		PercentMult = 300,
+	}
+
+	public class StatModifier
+	{
+		public readonly float Value;
+		public readonly StatModType Type;
+		public readonly int Order;
+		public readonly object Source;
+
+		public StatModifier(float value, StatModType type, int order, object source)
+		{
+			Value = value;
+			Type = type;
+			Order = order;
+			Source = source;
+		}
+
+		public StatModifier(float value, StatModType type) : this(value, type, (int)type, null) { }
+
+		public StatModifier(float value, StatModType type, int order) : this(value, type, order, null) { }
+
+		public StatModifier(float value, StatModType type, object source) : this(value, type, (int)type, source) { }
+	}
+}
