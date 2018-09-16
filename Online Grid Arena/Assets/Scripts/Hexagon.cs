dc43f55c1f571e4e2cc213f3cb519d531fbb98da
Ryan <rswllee@hotmail.com>
2018-09-16 18:56:18 -0400

    Added my own version of the hexagonal grid with full control from within the code. The hexagons are created out of 3d vectors opposed to using an asset and are built in the Game using a prefab and a triangles in the HexMesh class. HexGrid controls size and shape of the grid and is easily changeable. Hexagon is a static class that describes the size and shape of the hexagons.

---
+++ Online Grid Arena/Assets/Scripts/Hexagon.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//defines a hexagon by setting its side length and using that to descrbie the vectors creating it
+public static class Hexagon
+{
+    public const float outerRadius = 10f;
+    public const float innerRadius = outerRadius * 0.866025404f;
+
+    public static Vector3[] edges =
+    {
+        new Vector3(0f, 0f, outerRadius),
+        new Vector3(innerRadius, 0f, 0.5f * outerRadius),
+        new Vector3(innerRadius, 0f, -0.5f * outerRadius),
+        new Vector3(0f, 0f, -outerRadius),
+        new Vector3(-innerRadius, 0f, -0.5f * outerRadius),
+        new Vector3(-innerRadius, 0f, 0.5f * outerRadius),
+        new Vector3(0f, 0f, outerRadius)
+        //this line must be used so the loop making 6 triangles loops back to the first edge rather than overflowing
+    };
+}
