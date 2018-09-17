e7466825c81ed4b1b10b49d736f246d3ea5e40f6
lambhunter0 <george.theophanous@hotmail.com>
2018-09-17 18:59:24 -0400

    Prefab Grid implementation
    Created an alternate implementation for the grid, using gridMap and hextile.
    The grid is a 2d array of hexTiles, which are prefabs of hexagonal 3d objects with the hexTile script attached to them.
    TODO:
    -make one tile at a time clickable only
    -implement getNeighbor and get row/diagonal/column methods
    -clean up code
    -dependency injection

--- Online Grid Arena/Assets/Scripts/GridMap.cs
+++ Online Grid Arena/Assets/Scripts/GridMap.cs
@@ -4,46 +4,48 @@ using UnityEngine;
 
 public class GridMap : MonoBehaviour {
 
-    private int height = 6;
-    private int width = 6;
+    private int height = 7;
+    private int width = 7;
 
-    public GameObject tilePrefab_even;
-    public GameObject tilePrefab_odd;
+    //triple coordinate system
+    private double x; // x is column number coordinate 
+    private int colNum; // number of columns in total
+    public GameObject tilePrefab;
+    Vector3 tileSize;
+    //GameObject[,] grid;
+    GameObject[][] grid;
 
 	void Start () {
-
-        for (int x = 0; x < width; x++)
+        colNum = 7;
+        x = -3;
+        grid = new GameObject[colNum][];//num of cols
+        for (int i = 0; i < colNum; i++) // 0 to 6
         {
-            for (int y = 0; y < height; y++)
-            {
-                if ((x % 2) == 1) //if col is odd
-                {
-                    if ((y % 2) == 1) //if row is odd
-                    {
-                        Instantiate(tilePrefab_odd, new Vector3(x * 0.5f+0.26f, y*0.4f, 0), Quaternion.identity);
-                    }
-                    else //row even
-                    {
-                        Instantiate(tilePrefab_even, new Vector3(x * 0.5f, y * 0.4f, 0), Quaternion.identity);
-                    }
-                    
-                }
-                else //col even
-                {
-                    if ((y % 2) == 1) //if row is odd
-                    {
-                        Instantiate(tilePrefab_even, new Vector3(x * 0.5f+0.26f, y * 0.4f, 0), Quaternion.identity);
-                    }
-                    else //row even
-                    {
-                        Instantiate(tilePrefab_odd, new Vector3(x * 0.5f, y * 0.4f, 0), Quaternion.identity);
-                    }
-                }
-            }
+            grid[i] = new GameObject[7-Mathf.Abs((int)x)]; //column sizes: 4,5,6,7,6,5,4
+            x++;
         }
+
+        //grid = new GameObject[width, height];
+        tileSize = tilePrefab.GetComponent<Collider>().bounds.size;
+
+        // Debug.Log("tile on column "+x+" and row "+y+" has been instantiated at location (x,y)=("+x+","+y+")");
+
+
+        //here try to make a grid in hex shape
+        x = -3; //reset x so that we may reinstantiate at the right place? 
+        for (int index = 0; index < colNum; index++) //for each array of varying size of tiles
+        {
+            for (int j = 0; j < grid[index].Length; j++) //for each tile in that column
+            {
+                Instantiate(tilePrefab, new Vector3(index-(float)x/4, j+(Mathf.Abs((float)x)/2)), Quaternion.Euler(90, 0, 0));
+            }
+            x++;
+        }
+
 	}
 	
-	void Update () {
+	void Update ()
+    {
 		
 	}
 }
---
+++ Online Grid Arena/Assets/Scripts/HexTile.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexTile : MonoBehaviour {
+
+    Material currentMat;
+    public Material[] materials; //for now 0 is def and 1 is light blue
+    MeshRenderer rend;
+    Vector3 size;
+    bool isClicked;
+	// Use this for initialization
+	void Start ()
+    {
+        isClicked = false;
+        size = GetComponent<Collider>().bounds.size;
+        rend = this.GetComponentInChildren<MeshRenderer>();
+        currentMat = materials[0];
+        rend.material = currentMat;
+        Debug.Log(size.x+"x");
+        Debug.Log(size.y+"y");
+        Debug.Log(size.z+"z");
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+	}
+
+    private void OnMouseEnter()
+    {
+        if (!isClicked)
+        { 
+            currentMat = materials[1];
+            rend.material = currentMat;
+        }
+    }
+
+    private void OnMouseExit()
+    {
+        if (!isClicked)
+        {
+            currentMat = materials[0];
+            rend.material = currentMat;
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        Debug.Log(transform.position.y);
+        if (isClicked)
+        {
+            currentMat = materials[1];
+            rend.material = currentMat;
+            isClicked = !isClicked;
+        }
+        else
+        {
+            currentMat = materials[2];
+            rend.material = currentMat;
+            isClicked = !isClicked;
+        }
+    }
+
+}
