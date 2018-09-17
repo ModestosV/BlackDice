234a2d8d9e84869eba3db87a656aad24034a5362
Ryan <rswllee@hotmail.com>
2018-09-17 02:26:23 -0400

    Changed the coordinate system to have 3 coordinates instead of 2 in my version of hex grid

--- Online Grid Arena/Assets/Scripts/HexObject.cs
+++ Online Grid Arena/Assets/Scripts/HexObject.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class HexObject : MonoBehaviour
-{    
+{
+    //created a set of 3 coordinates
+    public HexCoordinates coordinates;
 
 }
