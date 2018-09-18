365dc66f61b96a0f3d47447c1d5f9bfa6d1af5c8
todd829 <patodd829@gmail.com>
2018-09-18 17:50:49 -0400

    Make MainMenu and changeScene+exitGame functions

---
+++ Online Grid Arena/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour {
+
+	public void changeScene(string sceneName)
+	{
+		Application.LoadLevel(sceneName);
+	}
+
+	public void exitGame()
+	{
+		Application.Quit();
+	}
+}
