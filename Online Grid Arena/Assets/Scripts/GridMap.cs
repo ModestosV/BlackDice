e6a0e9716b14eaa59f8be3a0ff1a5babeecc97af
lambhunter0 <george.theophanous@hotmail.com>
2018-09-17 20:37:46 -0400

    Implemented coordinate system
    X: represents column number (left most is -3, rightmost is 3)
    
    Y: represents diagonal from top left to bottom right, each tile on the same diagonal has the same y coordinate.  (bottom most diagonal is 3, top most is -3)
    
    Z: represents the diagonal from bottom left to top right (top most is 3, bottom most is -3)
    
    W: represents the row the tile is located on (topmost row is 12, bottom most row is 0)
    
    Note that these numbers can be easily adjusted to take in a larger grid size.

--- Online Grid Arena/Assets/Scripts/GridMap.cs
+++ Online Grid Arena/Assets/Scripts/GridMap.cs
@@ -9,15 +9,16 @@ public class GridMap : MonoBehaviour {
 
     //triple coordinate system
     private double x; // x is column number coordinate 
+    private int y, z, w; // for setting tile info
     private int colNum; // number of columns in total
     public GameObject tilePrefab;
     Vector3 tileSize;
-    //GameObject[,] grid;
     GameObject[][] grid;
 
 	void Start () {
         colNum = 7;
         x = -3;
+        y = 0;
         grid = new GameObject[colNum][];//num of cols
         for (int i = 0; i < colNum; i++) // 0 to 6
         {
@@ -25,19 +26,34 @@ public class GridMap : MonoBehaviour {
             x++;
         }
 
-        //grid = new GameObject[width, height];
         tileSize = tilePrefab.GetComponent<Collider>().bounds.size;
 
-        // Debug.Log("tile on column "+x+" and row "+y+" has been instantiated at location (x,y)=("+x+","+y+")");
-
-
-        //here try to make a grid in hex shape
-        x = -3; //reset x so that we may reinstantiate at the right place? 
+        x = -3; //reset x so that we may reinstantiate at the right place
         for (int index = 0; index < colNum; index++) //for each array of varying size of tiles
         {
+            w = Mathf.Abs((int)x);
+            if (x <= 0)
+            {
+                y = 3;
+                z = Mathf.Abs((int)x) - 3;
+            }
+            else
+            {
+                y = 3-(int)x;
+                z = -3;
+            }
             for (int j = 0; j < grid[index].Length; j++) //for each tile in that column
             {
-                Instantiate(tilePrefab, new Vector3(index-(float)x/4, j+(Mathf.Abs((float)x)/2)), Quaternion.Euler(90, 0, 0));
+                GameObject current = Instantiate(tilePrefab, new Vector3(index-(float)x/4, j+(Mathf.Abs((float)x)/2)), Quaternion.Euler(90, 0, 0));
+                HexTile tile = current.GetComponent<HexTile>();
+                tile.setX((int)x);
+                tile.setY((int)y);
+                y--;
+                tile.setZ(z);
+                z++;
+                tile.setW(w);
+                w++;
+                w++;
             }
             x++;
         }
@@ -48,4 +64,34 @@ public class GridMap : MonoBehaviour {
     {
 		
 	}
+
+    public GameObject[] getNeighbors() //returns all neighbors starting from bottom left, counter-clockwise
+    {
+        return null;
+    }
+
+    public GameObject[] getColumn() //returns entire column
+    {
+        return null;
+    }
+
+    public GameObject[] getRightDiagonal() //returns entire diagonal that goes from bottom left to top right
+    {
+        return null;
+    }
+
+    public GameObject[] getLeftDiagonal() //returns entire diagonal that goes from top left to bottom right
+    {
+        return null;
+    }
+
+    public GameObject[] getRowSkip() // returns entire row of tiles that are exactly at the same height (i.e. array will not hold tiles from every row)
+    {
+        return null;
+    }
+
+    public GameObject[] getRowFull() //return full row of tiles that are within +/= 0.5 of eachother (i.e. will have 1 tile from 1 row, 2 from the next, etc.etc.)
+    {
+        return null;
+    }
 }
