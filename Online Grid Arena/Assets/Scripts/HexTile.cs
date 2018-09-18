24188d4c298ead037a74c3648889819905bed4a4
lambhunter0 <george.theophanous@hotmail.com>
2018-09-17 20:37:46 -0400

    #1 Implemented coordinate system

--- Online Grid Arena/Assets/Scripts/HexTile.cs
+++ Online Grid Arena/Assets/Scripts/HexTile.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class HexTile : MonoBehaviour {
 
+
+    private int x, y, z; //x is column number, y is left diagonal, z is right diagonal all of them -3 to 3
+    private int w; //w is the "row" number (goes from 0 to 12 in the case of a grid with a middle column of height 7) because 7*2 -1 = 13 possible row placements
+    private GameObject occupant;
     Material currentMat;
     public Material[] materials; //for now 0 is def and 1 is light blue
     MeshRenderer rend;
@@ -12,14 +16,12 @@ public class HexTile : MonoBehaviour {
 	// Use this for initialization
 	void Start ()
     {
+        occupant = null;
         isClicked = false;
         size = GetComponent<Collider>().bounds.size;
         rend = this.GetComponentInChildren<MeshRenderer>();
         currentMat = materials[0];
         rend.material = currentMat;
-        Debug.Log(size.x+"x");
-        Debug.Log(size.y+"y");
-        Debug.Log(size.z+"z");
     }
 	
 	// Update is called once per frame
@@ -27,6 +29,11 @@ public class HexTile : MonoBehaviour {
     {
 
 	}
+
+    public void setCoords(int newX, int newY, int newZ, int newW)
+    {
+        
+    }
 
     private void OnMouseEnter()
     {
@@ -48,7 +55,6 @@ public class HexTile : MonoBehaviour {
 
     private void OnMouseDown()
     {
-        Debug.Log(transform.position.y);
         if (isClicked)
         {
             currentMat = materials[1];
@@ -61,6 +67,57 @@ public class HexTile : MonoBehaviour {
             rend.material = currentMat;
             isClicked = !isClicked;
         }
+        Debug.Log("TILE INFO: (column number)x = "+x+" /y = "+y+" /z = "+z+" /w = "+w);
+    }
+
+    public void setOccupant(GameObject occ)
+    {
+        occupant = occ;
+    }
+
+    public GameObject getOccupant()
+    {
+        return occupant;
+    }
+
+    public int getX()
+    {
+        return x;
+    }
+
+    public void setX(int newX)
+    {
+        x = newX;
+    }
+
+    public int getY()
+    {
+        return y;
+    }
+
+    public void setY(int newY)
+    {
+        y = newY;
+    }
+
+    public int getZ()
+    {
+        return z;
+    }
+
+    public void setZ(int newZ)
+    {
+        z = newZ;
+    }
+
+    public int getW()
+    {
+        return w;
+    }
+
+    public void setW(int newW)
+    {
+        w = newW;
     }
 
 }
