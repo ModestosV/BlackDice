489abc9905b166d4276a65d4e5c315174349ac49
Mathieu Leblanc <0mat.leb2@gmail.com>
2018-09-29 17:06:38 -0400

    Create scene with HUD and Character selection. Create Project folder to isolate project from thirdparty files. #4 #6

---
+++ Online Grid Arena/Assets/Project/Scripts/SelectionController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class SelectionController : MonoBehaviour {
+
+    [SerializeField] StatPanel statPanel;
+    public GameObject selectionHalo;
+    private Character selectedCharacter;
+
+	void Update () {
+        if (Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (!Physics.Raycast(ray, out hit) || hit.collider.gameObject.tag != "Player")
+            {
+                statPanel.gameObject.SetActive(false);
+                selectionHalo.gameObject.SetActive(false);
+                return;
+            }
+
+            statPanel.gameObject.SetActive(true);
+            selectedCharacter = hit.collider.gameObject.GetComponentInParent<Character>();
+            statPanel.SetStats(selectedCharacter.controller.Health, selectedCharacter.controller.Damage);
+            statPanel.UpdateStatValues();
+
+            selectionHalo.gameObject.SetActive(true);
+            selectionHalo.gameObject.transform.SetParent(hit.collider.gameObject.transform.parent.gameObject.transform);
+            selectionHalo.gameObject.transform.localPosition = new Vector3(0.0f, -0.99f, -1.0f);
+        }
+    }
+}
