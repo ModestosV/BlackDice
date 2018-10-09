a4408cd263f58b544ad963b73665a549193eaf18
todd829 <patodd829@gmail.com>
2018-10-09 14:44:42 -0400

    #5 Add MainMenu loop song and make option volume slider work

--- Online Grid Arena/Assets/Scripts/Main.cs
+++ Online Grid Arena/Assets/Scripts/Main.cs
@@ -10,6 +10,6 @@ public class Main : MonoBehaviour
 	
 	void Update ()
     {
-		
+
 	}
 }
--- Online Grid Arena/Assets/Scripts/MainMenu.cs
+++ Online Grid Arena/Assets/Scripts/MainMenu.cs
@@ -3,7 +3,6 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-
 	public void PlayGame()
 	{
 		SceneManager.LoadScene("Game");
