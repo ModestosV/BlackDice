296786e9d949ad6088c596379a8809a30977ce5a
Ryan <rswllee@hotmail.com>
2018-10-15 00:21:23 -0400

    #1 failing test, only known issue is on expected instantiation

--- Online Grid Arena/Assets/Editor/GridMapTests/ControllerTest.cs
+++ Online Grid Arena/Assets/Editor/GridMapTests/ControllerTest.cs
@@ -9,45 +9,52 @@ using System;
 public class ControllerTest
 {
     IGrid grid;
-    private GameObject gridID;
+    IHexTile hexTileController;
+
     private Dictionary<Tuple<int, int, int>, GameObject> gridDictionary;
-    GameObject tile;
-
-    //HexTileController tile = tileObject.GetComponent<HexTileController>();
-
     GridMapController gridMapController;
+    GameObject tileObject;
 
     [SetUp]
     public void Init()
     {
-        grid = Substitute.For<IGrid>();
-        gridID = Substitute.For<GameObject>();
-        //tileObject = Substitute.For<GameObject>();
+        tileObject = new GameObject();
+        GameObject[] expected = new GameObject[7];
 
-        //HexTileController tile = tileObject.GetComponent<HexTileController>();
-        //tile.x.Returns(0);
+        grid = Substitute.For<IGrid>();
+        hexTileController = Substitute.For<IHexTile>();
+
+        tileObject.AddComponent(typeof(HexTileController));
+    }
+
+    [Test]
+    public void is_this_working()
+    {
+        int i = 1;
+        int j = 1;
+        Assert.AreEqual(i, j);
     }
 
     [Test]
     public void Returns_proper_array_of_rows_or_columns()
     {
+        hexTileController.X.Returns(0);
+        
         GameObject[] expected = new GameObject[7];
-
-        expected[0] = gridDictionary[new Tuple<int, int, int> (0, 3, -3)];
-        expected[1] = gridDictionary[new Tuple<int, int, int>(0, 2, -2)];
-        expected[2] = gridDictionary[new Tuple<int, int, int>(0, 1, -1)];
+        expected[0] = gridDictionary[new Tuple<int, int, int>(0, -3, 3)];
+        expected[1] = gridDictionary[new Tuple<int, int, int>(0, -2, 2)];
+        expected[2] = gridDictionary[new Tuple<int, int, int>(0, -1, 1)];
         expected[3] = gridDictionary[new Tuple<int, int, int>(0, 0, 0)];
-        expected[4] = gridDictionary[new Tuple<int, int, int>(0, -1, 1)];
-        expected[5] = gridDictionary[new Tuple<int, int, int>(0, -2, 2)];
-        expected[6] = gridDictionary[new Tuple<int, int, int>(0, -3, 3)];
+        expected[4] = gridDictionary[new Tuple<int, int, int>(0, 1, -1)];
+        expected[5] = gridDictionary[new Tuple<int, int, int>(0, 2, -2)];
+        expected[6] = gridDictionary[new Tuple<int, int, int>(0, 3, -3)];
 
-        var actual = grid.getColumn(tile);
+        var actual = grid.getColumn(tileObject);
 
         for (int i = 0; i < 7; i++)
         {
-            Assert.Equals(actual[i], expected[i]);
+            Assert.AreEqual(expected[i], expected[i]);
         }
-
     }
 
 
