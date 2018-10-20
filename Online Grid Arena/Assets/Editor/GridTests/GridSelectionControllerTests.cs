20a565067500b47aaddc652fffa034b048fda969
Arkondoma <nchiassonpalacios@gmail.com>
2018-10-20 14:02:45 -0400

    Add unit tests for GridSelectionController

---
+++ Online Grid Arena/Assets/Editor/GridTests/GridSelectionControllerTests.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using NSubstitute;
+using NUnit.Framework;
+using UnityEngine;
+
+public class GridSelectionControllerTests
+{
+    GridSelectionController sut;
+
+    [SetUp]
+    public void Init()
+    {
+        sut = new GridSelectionController();
+        sut.Init();
+    }
+
+    [Test]
+    public void Add_Remove_hex_tile_to_Selected_Tiles()
+    {
+        IHexTile firstHexTile = Substitute.For<IHexTile>();
+        IHexTile secondHexTile = Substitute.For<IHexTile>();
+        HexTileController controller = new HexTileController();
+
+        firstHexTile.Controller().Returns(controller);
+        secondHexTile.Controller().Returns(controller);
+
+        // Add both tiles, remove first, only second is left
+        sut.AddSelectedTile(firstHexTile);
+        sut.AddSelectedTile(secondHexTile);
+        sut.RemoveSelectedTile(firstHexTile);
+        Assert.AreEqual(sut.SelectedTiles[0], secondHexTile);
+    }
+
+    [Test]
+    public void Add_Remove_hex_tile_to_Hovered_Tiles()
+    {
+        IHexTile firstHexTile = Substitute.For<IHexTile>();
+        IHexTile secondHexTile = Substitute.For<IHexTile>();
+        HexTileController controller = new HexTileController();
+
+        firstHexTile.Controller().Returns(controller);
+        secondHexTile.Controller().Returns(controller);
+
+        // Add both tiles, remove first, only second is left
+        sut.AddHoveredTile(firstHexTile);
+        sut.AddHoveredTile(secondHexTile);
+        sut.RemoveHoveredTile(firstHexTile);
+        Assert.AreEqual(sut.HoveredTiles[0], secondHexTile);
+    }
+
+    [Test]
+    public void Add_Remove_hex_tile_to_Path_Tiles()
+    {
+        IHexTile firstHexTile = Substitute.For<IHexTile>();
+        IHexTile secondHexTile = Substitute.For<IHexTile>();
+        HexTileController controller = new HexTileController();
+
+        firstHexTile.Controller().Returns(controller);
+        secondHexTile.Controller().Returns(controller);
+
+        // Add both tiles, remove first, only second is left
+        sut.AddPathTile(firstHexTile);
+        sut.AddPathTile(secondHexTile);
+        sut.RemovePathTile(firstHexTile);
+        Assert.AreEqual(sut.PathTiles[0], secondHexTile);
+    }
+
+    [Test]
+    public void Deselect_all_Selected_Tiles()
+    {
+        IHexTile firstHexTile = Substitute.For<IHexTile>();
+        IHexTile secondHexTile = Substitute.For<IHexTile>();
+        HexTileController controller = Substitute.For<HexTileController>();
+
+        firstHexTile.Controller().Returns(controller);
+        secondHexTile.Controller().Returns(controller);
+
+        sut.AddSelectedTile(firstHexTile);
+        sut.AddSelectedTile(secondHexTile);
+        sut.DeselectAll();
+
+        controller.Received(2).Deselect();
+    }
+
+    [Test]
+    public void Blur_all_Hovered_Tiles()
+    {
+        IHexTile firstHexTile = Substitute.For<IHexTile>();
+        IHexTile secondHexTile = Substitute.For<IHexTile>();
+        HexTileController controller = Substitute.For<HexTileController>();
+
+        firstHexTile.Controller().Returns(controller);
+        secondHexTile.Controller().Returns(controller);
+
+        sut.AddHoveredTile(firstHexTile);
+        sut.AddHoveredTile(secondHexTile);
+        sut.BlurAll();
+
+        controller.Received(2).Blur();
+    }
+
+    [Test]
+    public void Scrub_all_Path_Tiles()
+    {
+        IHexTile firstHexTile = Substitute.For<IHexTile>();
+        IHexTile secondHexTile = Substitute.For<IHexTile>();
+        HexTileController controller = Substitute.For<HexTileController>();
+
+        firstHexTile.Controller().Returns(controller);
+        secondHexTile.Controller().Returns(controller);
+
+        sut.AddPathTile(firstHexTile);
+        sut.AddPathTile(secondHexTile);
+        sut.ScrubPathAll();
+
+        controller.Received(2).ScrubPath();
+    }
+
+    [Test]
+    public void Draw_all_Path_Tiles()
+    {
+        IHexTile firstHexTile = Substitute.For<IHexTile>();
+        IHexTile secondHexTile = Substitute.For<IHexTile>();
+        HexTileController controller = Substitute.For<HexTileController>();
+
+        firstHexTile.Controller().Returns(controller);
+        secondHexTile.Controller().Returns(controller);
+
+        sut.AddPathTile(firstHexTile);
+        sut.AddPathTile(secondHexTile);
+        sut.DrawPath(sut.PathTiles);
+
+        controller.Received(2).MarkPath();
+    }
+}
