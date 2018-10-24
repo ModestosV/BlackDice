ae2414dac6b46acfa445db1d45d615ea0e849d31
Mathieu Leblanc <0mat.leb2@gmail.com>
2018-10-24 12:18:02 -0400

    Create menu UI for login and registration. #13

---
+++ Online Grid Arena/Assets/Scripts/MainMenu/LoginButton.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class LoginButton : MonoBehaviour {
+
+    public TextMeshProUGUI statusText;
+    public TextMeshProUGUI emailText;
+    public TextMeshProUGUI passwordText;
+
+    public void Login()
+    {
+        string email = emailText.text;
+        string password = passwordText.text;
+
+        if (!email.Contains("@") || password.Length < 8)
+        {
+            SetInvalidCredentialsStatusMessage();
+            return;
+        }
+
+        SetSuccessfulLoginStatusMessage();
+    }
+    
+    private void SetInvalidCredentialsStatusMessage()
+    {
+        statusText.text = "The email and password combination you have entered is ridiculously incorrect. Try again... or don't.";
+    }
+
+    private void SetSuccessfulLoginStatusMessage()
+    {
+        statusText.text = "You have been logged in successfully. I am so impressed.";
+    }
+}
