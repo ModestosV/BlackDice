80a6774dd1c65efeab3f0a75ff663f0af5dc188f
Marc Dragon <thedragon_master@hotmail.ca>
2018-10-24 14:27:08 -0400

    Changed the test
    #13

--- Server/src/tests/server.test.ts
+++ Server/src/tests/server.test.ts
@@ -1,14 +1,9 @@
 const app = require('../server');
 import request from 'supertest'
-let server: request.SuperTest<request.Test>;
-
-beforeAll(() => {
-  server = request(app);
-})
 
 describe('Testing the inital call to the root location of the server', () => {
   test( 'Root Call', async () => { 
-     const response = await server.get('/');
+     const response = await request(app).get('/');
      expect(response.status).toBe(200);
   })
 });
