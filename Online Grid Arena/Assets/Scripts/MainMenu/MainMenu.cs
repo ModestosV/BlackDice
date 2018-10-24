53305c965306f6a08a2cbbbf1c42f5832708b6c0
Mathieu Leblanc <0mat.leb2@gmail.com>
2018-10-24 02:25:21 -0400

    Set up Online menu in game. Fetch random text from web API and display. #13

--- Online Grid Arena/Assets/Scripts/MainMenu/MainMenu.cs
+++ Online Grid Arena/Assets/Scripts/MainMenu/MainMenu.cs
@@ -1,8 +1,13 @@
 ﻿using UnityEngine;
+using UnityEngine.Networking;
 using UnityEngine.SceneManagement;
+using System.Collections;
+using TMPro;
 
 public class MainMenu : MonoBehaviour
 {
+    public TextMeshProUGUI webText;
+
 	public void PlayGame()
 	{
 		SceneManager.LoadScene(1);
@@ -12,4 +17,31 @@ public class MainMenu : MonoBehaviour
 	{
 		Application.Quit();
 	}
+
+    public void FetchWebData()
+    {
+        StartCoroutine(GetTextFromWeb());
+    }
+
+    IEnumerator GetTextFromWeb()
+    {
+        using (UnityWebRequest www = UnityWebRequest.Get("http://www.randomtext.me/api/"))
+        {
+            yield return www.SendWebRequest();
+
+            if (www.isNetworkError || www.isHttpError)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                // Show results as text
+                Debug.Log(www.downloadHandler.text);
+                webText.text = www.downloadHandler.text;
+
+                // Or retrieve results as binary data
+                byte[] results = www.downloadHandler.data;
+            }
+        }
+    }
 }
