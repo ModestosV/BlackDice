adb81cca2241800e00d119f96852dd4993b32b74
Mathieu Leblanc <0mat.leb2@gmail.com>
2018-10-25 12:23:37 -0400

    Adding username to user schema
    #13 Updating the schema

--- Server/src/models/User.ts
+++ Server/src/models/User.ts
@@ -4,6 +4,7 @@ const Types = mongoose.Schema.Types;
 const userSchema = new mongoose.Schema({
   createdAt: { type: Types.Date, required: true },
   email: { type: Types.String, required: true, unique: true },
+  username: { type: Types.String, required: true, unique: true },
   givenname: { type: Types.String, required: true },
   passwordHash: { type: Types.String, required: true },
   surname: { type: Types.String, required: true },
