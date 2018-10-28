9cd0a07b821f543bc465f8f8a9b57738fa1c8ce6
Mathieu Leblanc <0mat.leb2@gmail.com>
2018-10-28 15:35:09 -0400

    Clear unused node modules. #13

--- Server/dist/src/utils/middlewares.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function errorHandler(err, req, res, next) {
-    global.console.error('Error:');
-    global.console.error(err);
-    return res.json(err);
-}
-exports.errorHandler = errorHandler;
-//# sourceMappingURL=middlewares.js.map
--- Server/src/utils/middlewares.ts
+++
@@ -1,8 +0,0 @@
-import express, { NextFunction, Request, Response } from 'express';
-
-export function errorHandler(err: Error, req: Request, res: Response, next: NextFunction) {
-  global.console.error('Error:');
-  global.console.error(err);
-  return res.json(err);
-}
-
