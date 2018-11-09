376308faa7afffbe2ed2262c60c0b90f969a3903
Marc Dragon <thedragon_master@hotmail.ca>
2018-11-09 17:29:00 -0500

    finished dotenv addition

--- Server/src/server.ts
+++ Server/src/server.ts
@@ -1,14 +1,17 @@
 // Starting the server code
 import consoleStamp from "console-stamp";
+import dotenv from "dotenv";
 import { NextFunction, Request, Response } from "express";
 import * as httpServer from "http";
 import app from "./app";
 
-const stamp = { pattern: "UTC:yyyy-mm-dd'T'HH:MM:ss".toString() };
+const stamp = { pattern: "UTC:yyyy-mm-dd'T'HH:MM:ss" };
+
+dotenv.config();
 
 let server = null;
-const host = "localhost";
-const port = 5500;
+const host = process.env.HOST || "localhost";
+const port = parseInt(process.env.PORT ? process.env.PORT : "5500", 10);
 
 consoleStamp(console, stamp);
 
