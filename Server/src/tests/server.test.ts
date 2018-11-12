4e3e898c9db6d6cf9bff1c458fce4df7bf4bee11
Marc Dragon <thedragon_master@hotmail.ca>
2018-11-11 23:31:25 -0500

    Added server unit tests
    #42 #53

--- Server/src/tests/server.test.ts
+++ Server/src/tests/server.test.ts
@@ -1,10 +1,56 @@
 import request from "supertest";
+import app from "../app";
+import User from "../app/models";
 
 describe("Testing the inital call to the root location of the server", () => {
-    test("Root Call", async () => {
-        const app = require("../app");
+    test("Route that doesn't exist", async () => {
         const conn = request(app);
         const response = await conn.get("/");
-        expect(response.status).toBe(200);
+        expect(response.status).toBe(404);
+    });
+
+    test("Test Register Router Improperly", () => {
+        const conn = request(app);
+        conn.post("/account/register").expect(400);
+    });
+
+    test("Test Register Router", async (done) => {
+        const json = {
+            email: "test@marc2.com",
+            password: "Thiscleartextisbad",
+            username: "marc2"
+        };
+        const conn = request(app);
+        spyOn(User("User"), "create").and.returnValue(Promise.resolve(json));
+        conn.post("/account/register")
+            .send(json)
+            .set("Content-Type", "application/json")
+            .expect(200)
+            .end((err) => {
+                if (err) {
+                    return done(err);
+                }
+                return done();
+            });
+    });
+
+    test("Test Register Router", async (done) => {
+        const json = {
+            email: "test@marc2.com",
+            password: "Thiscleartextisbad",
+            username: "marc2"
+        };
+        const conn = request(app);
+        spyOn(User("User"), "create").and.returnValue(Promise.resolve(json));
+        conn.post("/account/register")
+            .send(json)
+            .set("Content-Type", "application/json")
+            .expect(412)
+            .end((err) => {
+                if (err) {
+                    return done(err);
+                }
+                return done();
+            });
     });
 });
