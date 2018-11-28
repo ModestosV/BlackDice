d7eea92a6c3d0b9d1dd3a0cc05275763f171f485
Mathieu Leblanc <0mat.leb2@gmail.com>
2018-11-28 03:51:35 -0500

    Create React components for downloads and quote of the day. Design responsive layout with CSS Grid. #55

--- blackdice-website/src/App.js
+++ blackdice-website/src/App.js
@@ -1,28 +1,73 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Downloads from './Components/Downloads'
 import './App.css';
+import QuoteOfTheDay from './Components/QuoteOfTheDay';
 
 class App extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      downloads: [],
+      quoteOfTheDay: {}
+    }
+  }
+
+  componentDidMount() {
+    this.getDownloads()
+    this.getQuoteOfTheDay()
+  }
+
+  getQuoteOfTheDay() {
+    fetch('https://quotes.rest/qod')
+      .then(response => response.json())
+      .then(json => {
+        this.setState({quoteOfTheDay: json.contents.quotes[0]})
+      })
+  }
+  
+  getDownloads() {
+    this.setState({downloads: [
+      {
+        title: "v0.1_Windows_x86",
+        link: "https://github.com/ModestosV/BlackDice/releases/download/Iteration4/Online_Grid_Arena_Setup_x86.exe" 
+      }
+    ]})
+  }
+
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+
+        <div className="Title">
+          <h1>Black Dice</h1>
+        </div>
+
+        <div className="Navbar">
+          Navbar
+        </div>
+
+        <div className="DownloadsHeader">
+          <h2>Online Grid Arena Downloads</h2>
+        </div>
+
+        <Downloads downloads={this.state.downloads}/>
+
+        <QuoteOfTheDay quote={this.state.quoteOfTheDay}/>
+
+        <div className="Sidebar">
+          Sidebar
+        </div>
+
+        <div className="Footer">
+          Footer
+        </div>
+
+      </div> /* .App */
+    )
   }
+
 }
 
 export default App;
