8f5a28f03387d407a7a344af05eaca82ec5bbb91
Unknown <thedragon_master@hotmail.ca>
2018-12-30 14:30:44 -0500

    fixing indentation

--- Online Grid Arena/Assets/Scripts/Loggers/Loggly.cs
+++ Online Grid Arena/Assets/Scripts/Loggers/Loggly.cs
@@ -14,39 +14,39 @@ public class Loggly : MonoBehaviour {
 		
 	}
 
-	//Register the HandleLog function on scene start to fire on debug.log events
-    public void OnEnable(){
-	    Application.logMessageReceived += HandleLog;
-    }
+  //Register the HandleLog function on scene start to fire on debug.log events
+  public void OnEnable(){
+    Application.logMessageReceived += HandleLog;
+  }
 
-    //Remove callback when object goes out of scope
-    public void OnDisable(){
-	    Application.logMessageReceived -= HandleLog;
-    }
+  //Remove callback when object goes out of scope
+  public void OnDisable(){
+    Application.logMessageReceived -= HandleLog;
+  }
 
-    //Create a string to store log level in
-    string level = "";
+  //Create a string to store log level in
+  string level = "";
 
-    //Capture debug.log output, send logs to Loggly
-    public void HandleLog(string logString, string stackTrace, LogType type) {
+  //Capture debug.log output, send logs to Loggly
+  public void HandleLog(string logString, string stackTrace, LogType type) {
 
-      //Initialize WWWForm and store log level as a string
-      level = type.ToString ();
-      var loggingForm = new WWWForm();
+    //Initialize WWWForm and store log level as a string
+    level = type.ToString ();
+    var loggingForm = new WWWForm();
 
-      //Add log message to WWWForm
-      loggingForm.AddField("LEVEL", level);
-      loggingForm.AddField("Message", logString);
-      loggingForm.AddField("Stack_Trace", stackTrace);
+    //Add log message to WWWForm
+    loggingForm.AddField("LEVEL", level);
+    loggingForm.AddField("Message", logString);
+    loggingForm.AddField("Stack_Trace", stackTrace);
 
-      //Add any User, Game, or Device MetaData that would be useful to finding issues later
-      loggingForm.AddField("Device_Model", SystemInfo.deviceModel);
-      StartCoroutine(SendData(loggingForm));
-	}
+    //Add any User, Game, or Device MetaData that would be useful to finding issues later
+    loggingForm.AddField("Device_Model", SystemInfo.deviceModel);
+    StartCoroutine(SendData(loggingForm));
+  }
 
-    public IEnumerator SendData(WWWForm form){
-      //Send WWW Form to Loggly, replace TOKEN with your unique ID from Loggly
-      WWW sendLog = new WWW(URLs.URL_LOGGLY, form);
-      yield return sendLog;
-    }
+  public IEnumerator SendData(WWWForm form){
+    //Send WWW Form to Loggly
+    WWW sendLog = new WWW(URLs.URL_LOGGLY, form);
+    yield return sendLog;
+  }
 }
