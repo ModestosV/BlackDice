b28278f0fb28e3f876f6274df85e4db618c41758
Unknown <thedragon_master@hotmail.ca>
2018-12-29 21:03:57 -0500

    Started the loggly integration

--- Online Grid Arena/Assets/Scripts/Globals/URLs.cs
+++ Online Grid Arena/Assets/Scripts/Globals/URLs.cs
@@ -3,4 +3,6 @@
     // Developers by default will have requests pointed to local server. Users by default will have requests pointed to remote server.
     private static readonly string env = System.Environment.GetEnvironmentVariable("BLACK_DICE", System.EnvironmentVariableTarget.Machine);
     public static readonly string BASE_URL = string.Equals(env, "dev") ? "http://localhost:5500" : "http://159.203.27.234:5500";
+    private static readonly string USER_TOKEN = "20a4c6dd-a733-4a56-bbf5-0c05ff1d17cb";
+    public static readonly string URL_LOGGLY = "http://logs-01.loggly.com/inputs/"+ USER_TOKEN + "/tag/Unity3D"; 
 }
---
+++ Online Grid Arena/Assets/Scripts/Loggers/Loggly.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loggly : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	//Register the HandleLog function on scene start to fire on debug.log events
+    public void OnEnable(){
+	    Application.logMessageReceived += HandleLog;
+    }
+
+    //Remove callback when object goes out of scope
+    public void OnDisable(){
+	    Application.logMessageReceived -= HandleLog;
+    }
+
+    //Create a string to store log level in
+    string level = "";
+
+    //Capture debug.log output, send logs to Loggly
+    public void HandleLog(string logString, string stackTrace, LogType type) {
+
+      //Initialize WWWForm and store log level as a string
+      level = type.ToString ();
+      var loggingForm = new WWWForm();
+
+      //Add log message to WWWForm
+      loggingForm.AddField("LEVEL", level);
+      loggingForm.AddField("Message", logString);
+      loggingForm.AddField("Stack_Trace", stackTrace);
+
+      //Add any User, Game, or Device MetaData that would be useful to finding issues later
+      loggingForm.AddField("Device_Model", SystemInfo.deviceModel);
+      StartCoroutine(SendData(loggingForm));
+	}
+
+    public IEnumerator SendData(WWWForm form){
+      //Send WWW Form to Loggly, replace TOKEN with your unique ID from Loggly
+      WWW sendLog = new WWW(URLs.URL_LOGGLY, form);
+      yield return sendLog;
+    }
+}
