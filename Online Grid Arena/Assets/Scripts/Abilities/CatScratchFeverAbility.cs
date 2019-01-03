d8439d43a8087372d0724abdcfc29b1018c303f2
lambhunter0 <george.theophanous@hotmail.com>
2019-01-03 15:02:47 -0500

    added the catscratchfeverability that inherits from passive ability. it applies the effect to the appropriate tile #83

---
+++ Online Grid Arena/Assets/Scripts/Abilities/CatScratchFeverAbility.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class CatScratchFeverAbility : PassiveAbility
+{
+    public CatScratchFeverAbility() : base(
+         AbilityType.PASSIVE, 
+         new CatScratchFever()
+         )
+    {
+
+    }
+
+    public override void Execute(IHexTileController targetTile) => effect.Apply(targetTile);
+}
