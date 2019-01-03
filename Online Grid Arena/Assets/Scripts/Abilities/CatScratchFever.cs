a28a96e9d87e1683c83f4a8706073cf7fdf3f3d0
lambhunter0 <george.theophanous@hotmail.com>
2019-01-03 14:37:17 -0500

    catscratchFever tells targetTile to apply effect #83

--- Online Grid Arena/Assets/Scripts/Abilities/CatScratchFever.cs
+++ Online Grid Arena/Assets/Scripts/Abilities/CatScratchFever.cs
@@ -14,11 +14,7 @@ public sealed class CatScratchFever : StackModifier
 
     public override void Apply(IHexTileController targetTile)
     {
-        //apply the effect 
-        durationRemaining = duration; //make the duration refresh
-        //make the effect be applied to character. this means give tile a way to pass it to character or smth
-        //character will take care of applying the necessary effects? and checkign if stack num is too high
+        durationRemaining = duration;
+        targetTile.ApplyEffect(this);
     }
-
-    //the character will take care of removing stacks n shit
 }
