96d392b2f5f939a824710fda8fb2e8f0b4ec9359
lambhunter0 <george.theophanous@hotmail.com>
2019-01-03 15:47:42 -0500

    added reference to modified stat and to number of stacks #83

--- Online Grid Arena/Assets/Scripts/Abilities/StackModifier.cs
+++ Online Grid Arena/Assets/Scripts/Abilities/StackModifier.cs
@@ -4,11 +4,15 @@ public abstract class StackModifier : AbstractEffect
 {
     protected float buffPerStack;
     protected int maxStacks;
+    protected string statModifier;
+    protected int stacks;
 
-    protected StackModifier(EffectType type, int duration, int buffPerStack, int maxStacks) : base(type, duration)
+    protected StackModifier(EffectType type, int duration, int buffPerStack, int maxStacks, string statModifier) : base(type, duration)
     {
         this.Type = type;
         this.buffPerStack = buffPerStack;
         this.maxStacks = maxStacks;
+        this.statModifier = statModifier;
+        this.stacks = 1;
     }
 }
