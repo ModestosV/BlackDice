1cb623c14b351f1469d20587574182848f63ba7e
lambhunter0 <george.theophanous@hotmail.com>
2019-01-03 15:01:44 -0500

    created a passive ability class that inherits from abstract ability. its only parameter is the effect that it applies to whomever. this logic will be taken care of in the ability itself.#83

---
+++ Online Grid Arena/Assets/Scripts/Abilities/PassiveAbility.cs
@@ -0,0 +1,12 @@
+﻿
+using UnityEngine;
+
+public abstract class PassiveAbility : AbstractAbility
+{
+    protected AbstractEffect effect;
+
+    protected PassiveAbility(AbilityType type,AbstractEffect effect) : base(type, cooldown)
+    {
+        this.effect = effect;
+    }
+}
