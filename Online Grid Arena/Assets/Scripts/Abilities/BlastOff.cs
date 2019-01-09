ec92a3c982a087b2772bd9d9501d7e045a9432b9
Nicholas <nchiassonpalacios@gmail.com>
2019-01-09 18:54:47 -0500

    #71 Initial setup for new ability BlastOff

---
+++ Online Grid Arena/Assets/Scripts/Abilities/BlastOff.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class BlastOff : TargetedAbility {
+
+    public BlastOff() : base(
+        AbilityType.ACTIVATED,
+        1,
+        25.0f,
+        10,
+        Resources.Load<GameObject>("Prefabs/AbilityAnimations/ScratchAnimation"),
+        Resources.Load<AudioClip>("Audio/Ability/CottonRip")
+        )
+    {
+
+    }
+
+    public override void Execute(IHexTileController targetTile)
+    {
+        // check if target tile is open
+        if (!targetTile.IsOccupied())
+        {
+
+        }
+        else Debug.Log("Tile is occupied");
+        // trigger animation
+        // move character to targeted tile
+        // deal damage to all tiles around new location
+    }
+}
