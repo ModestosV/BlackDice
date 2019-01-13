5adc8de9a108521c9dc41a5c9e12406e97eb0e83
lambhunter0 <george.theophanous@hotmail.com>
2019-01-13 15:50:50 -0500

    removed unused classes #83

--- Online Grid Arena/Assets/Scripts/HUD/Interfaces/IEffectPanel.cs
+++
@@ -1,4 +0,0 @@
-﻿public interface IEffectPanel : IMonoBehaviour
-{
-    IEffectPanelController Controller { get; }
-}
--- Online Grid Arena/Assets/Scripts/HUD/Interfaces/IEffectPanelController.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Collections.Generic;
-
-public interface IEffectPanelController
-{
-
-}
