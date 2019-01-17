6f1b0acadfc5e602ccbff9d1e3a8389f17e263ed
Ryan <rswllee@hotmail.com>
2019-01-17 00:31:22 -0500

    Added new game version to site

--- react-app/src/Components/TermsOfServicePopUp.js
+++ react-app/src/Components/TermsOfServicePopUp.js
@@ -2,14 +2,11 @@ import React, { Component } from 'react'
 
 class TermsOfServicePopUp extends Component {
 	render(){
-		return
-			<
-				input type="text"
-				placeholder={this.props.placeholder}
-				className = "mm-popup)_input"
-				value={this.state.value}
-				onChange={this.onChange}
-			/>;
+		return(
+			<div className="TermsOfServicePopUp">
+			
+			</div>
+		);
 	}
 }
 
