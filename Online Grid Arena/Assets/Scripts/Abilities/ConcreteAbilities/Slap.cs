ee5928bc891151244c9bd36d1d664a92c4d611c4
lambhunter0 <george.theophanous@hotmail.com>
2019-01-19 18:46:16 -0500

    #99 added slap ability and characterIcon

---
+++ Online Grid Arena/Assets/Scripts/Abilities/ConcreteAbilities/Slap.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public sealed class Slap : AbstractTargetedAbility
+{
+    public Slap(Pengwin activeCharacter) : base(
+        Resources.Load<Sprite>("Sprites/cursorSword_gold"),
+        Resources.Load<GameObject>("Assets/Resources/Prefabs/AbilityAnimations/SlapAnimation"),
+        Resources.Load<AudioClip>("Audio/Ability/CottonRip"),
+        activeCharacter,
+        1,
+        1,
+        AbilityType.TARGET_ENEMY)
+    {    }
+
+    protected override void PrimaryAction(List<IHexTileController> targetTiles)
+    {
+        targetTiles[0].Damage(character.Controller.CharacterStats["attack"].Value);
+        PlaySoundEffect();
+        PlayAnimation(targetTiles[0]);
+    }
+}
--- Online Grid Arena/Assets/Scripts/Character/Pengwin.cs
+++ Online Grid Arena/Assets/Scripts/Character/Pengwin.cs
@@ -20,12 +20,24 @@ public sealed class Pengwin : AbstractCharacter
 
         var effects = new List<IEffect>() { };
 
-        IAbility placeholder1 = new Placeholder(this);
+        IAbility slap = new Slap(this);
         IAbility placeholder2 = new Placeholder(this);
         IAbility placeholder3 = new Placeholder(this);
         IAbility placeholder4 = new Placeholder(this);
 
-        var abilities = new List<IAbility>() { placeholder1, placeholder2, placeholder3, placeholder4 };
+        var abilities = new List<IAbility>() { slap, placeholder2, placeholder3, placeholder4 };
+
+        characterController = new CharacterController()
+        {
+            Character = this,
+            OwnedByPlayer = playerName,
+            CharacterIcon = characterIcon,
+            BorderColor = borderColor,
+            HealthBar = GetComponentInChildren<HealthBar>(),
+            Abilities = abilities,
+            CharacterStats = characterStats,
+            Effects = effects
+        };
     }
 
     void Start()
