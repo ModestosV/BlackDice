b7bf51d98a4703ce5d13d0c9a9fcfdd0efc570b1
lambhunter0 <george.theophanous@hotmail.com>
2019-01-19 19:33:13 -0500

    #99  added sound, animation, and percent chance to trigger again

--- Online Grid Arena/Assets/Scripts/Abilities/ConcreteAbilities/Slap.cs
+++ Online Grid Arena/Assets/Scripts/Abilities/ConcreteAbilities/Slap.cs
@@ -1,12 +1,14 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public sealed class Slap : AbstractTargetedAbility
 {
+    private System.Random randomizer = new System.Random();
     public Slap(Pengwin activeCharacter) : base(
         Resources.Load<Sprite>("Sprites/cursorSword_gold"),
-        Resources.Load<GameObject>("Assets/Resources/Prefabs/AbilityAnimations/SlapAnimation"),
-        Resources.Load<AudioClip>("Audio/Ability/CottonRip"),
+        Resources.Load<GameObject>("Prefabs/AbilityAnimations/SlapAnimation"),
+        Resources.Load<AudioClip>("Audio/Ability/slap"),
         activeCharacter,
         1,
         1,
@@ -15,8 +17,20 @@ public sealed class Slap : AbstractTargetedAbility
 
     protected override void PrimaryAction(List<IHexTileController> targetTiles)
     {
+        ChanceToTrigger(75, targetTiles);
+    }
+
+    private void ChanceToTrigger(int chance, List<IHexTileController> targetTiles)
+    {
         targetTiles[0].Damage(character.Controller.CharacterStats["attack"].Value);
         PlaySoundEffect();
         PlayAnimation(targetTiles[0]);
+
+        System.Random randomizer = new System.Random();
+        int rand = randomizer.Next(0,100);
+        if (rand < chance)
+        {
+            ChanceToTrigger(chance-25, targetTiles);
+        }
     }
 }
