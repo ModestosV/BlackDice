cb19e425a5f27eec251525237bb98c51ff2044af
lambhunter0 <george.theophanous@hotmail.com>
2019-01-22 21:17:56 -0500

    #125 fix spaghetti code

--- Online Grid Arena/Assets/Scripts/Selection/TargetLineAOEAbilitySelectionController.cs
+++ Online Grid Arena/Assets/Scripts/Selection/TargetLineAOEAbilitySelectionController.cs
@@ -17,54 +17,16 @@ public class TargetLineAOEAbilitySelectionController : TargetLineAbilitySelectio
     protected override void DoHoverUnoccupiedTile()
     {
         base.DoHoverUnoccupiedTile();
-        bool isError = false;
-        IHexTileController selectedTile = GridSelectionController.SelectedTile;
-        List<IHexTileController> path = selectedTile.GetPath(inputParameters.TargetTile, true);
-        if (path.Count == 2)
-        {
-            DehighlightNeighbors(path[0]);
-            HighlightAffectedTiles(path[1]);
-            return;
-        }
-        for (int i = 0; i < path.Count; i++)
-        {
-            if (selectedTile.X != path[i].X || selectedTile.Y != path[i].Y || selectedTile.Z != path[i].Z)
-            {
-                isError = true;
-            }
-        }
-        if (path.Count > 2)
-        {
-            DehighlightNeighbors(path[path.Count - 2]);
-            HighlightAffectedTiles(path[path.Count - 1]);
-            if (isError)
-            {
-                path[path.Count - 3].HoverError();
-            }
-            else
-            {
-                path[path.Count - 3].HoverError();
-            }
-        }
-        path[path.Count - 1].Hover();
-
+        HighlightAffectedTiles(inputParameters.TargetTile);
     }
 
     private void HighlightAffectedTiles(IHexTileController target)
     {
+        target.Dehighlight();
         foreach (IHexTileController affected in target.GetNeighbors())
         {
             affected.HoverError();
         }
         target.Hover();
-    }
-
-    private void DehighlightNeighbors(IHexTileController target)
-    {
-        target.HoverError();
-        foreach (IHexTileController affected in target.GetNeighbors())
-        {
-            affected.Dehighlight();
-        }
     }
 }
