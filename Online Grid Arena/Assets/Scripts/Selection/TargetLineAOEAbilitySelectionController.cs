7cfe56d284086000673121566613e391daab0ab6
lambhunter0 <george.theophanous@hotmail.com>
2019-01-22 20:52:42 -0500

    #125 import assets, create new selection controller, selection works fine for Kamikaze, and create kamikaze ability

---
+++ Online Grid Arena/Assets/Scripts/Selection/TargetLineAOEAbilitySelectionController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetLineAOEAbilitySelectionController : TargetLineAbilitySelectionController
+{
+    protected override void DoHoverOccupiedTile()
+    {
+        base.DoHoverOccupiedTile();
+        IHexTileController selectedTile = GridSelectionController.SelectedTile;
+        List<IHexTileController> path = selectedTile.GetPath(inputParameters.TargetTile, true);
+        if (path.Count >= 2)
+        {
+            HighlightAffectedTiles(path[path.Count - 2]);
+        }
+    }
+
+    protected override void DoHoverUnoccupiedTile()
+    {
+        base.DoHoverUnoccupiedTile();
+        IHexTileController selectedTile = GridSelectionController.SelectedTile;
+        List<IHexTileController> path = selectedTile.GetPath(inputParameters.TargetTile, true);
+        if (path.Count > 2)
+        {
+            DehighlightNeighbors(path[path.Count - 2], path[path.Count - 3]);
+            HighlightAffectedTiles(path[path.Count - 1]);
+        }
+        path[path.Count - 1].Hover();
+    }
+
+    private void HighlightAffectedTiles(IHexTileController target)
+    {
+        foreach (IHexTileController affected in target.GetNeighbors())
+        {
+            affected.HoverError();
+        }
+        target.Hover();
+    }
+
+    private void DehighlightNeighbors(IHexTileController target, IHexTileController exception)
+    {
+        target.HoverError();
+        foreach (IHexTileController affected in target.GetNeighbors())
+        {
+            if (affected != exception)
+            {
+                affected.Dehighlight();
+            }
+        }
+    }
+}
