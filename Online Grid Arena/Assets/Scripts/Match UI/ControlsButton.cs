a201991bb66f551f92444fda2c6d88bfead7c984
lambhunter0 <george.theophanous@hotmail.com>
2019-01-23 23:14:52 -0500

    #108 menu exists and works, just need to populate

---
+++ Online Grid Arena/Assets/Scripts/Match UI/ControlsButton.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public sealed class ControlsButton : MonoBehaviour
+{
+    public IControlsMenu ControlsMenu { private get; set; }
+    private Button button;
+
+    void OnValidate()
+    {
+        button = GetComponent<Button>();
+        ControlsMenu = FindObjectOfType<ControlsMenu>();
+    }
+
+    void Awake()
+    {
+        button = GetComponent<Button>();
+        ControlsMenu = FindObjectOfType<ControlsMenu>();
+        button.onClick.AddListener(ToggleControlsMenu);
+    }
+
+    private void ToggleControlsMenu()
+    {
+        ControlsMenu.Toggle();
+    }
+}
---
+++ Online Grid Arena/Assets/Scripts/Match UI/ControlsMenu.cs
@@ -0,0 +1,29 @@
+﻿public class ControlsMenu : HideableUI, IControlsMenu
+{ 
+    private bool visible;
+
+    void OnValidate()
+    {
+        Init();
+    }
+
+    void Awake()
+    {
+        Init();
+        Hide();
+    }
+
+    public void Toggle()
+    {
+        visible = !visible;
+
+        if (!visible)
+        {
+            Hide();
+        }
+        else
+        {
+            Show();
+        }
+    }
+}
