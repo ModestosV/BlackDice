b257f624ba2bf6764c345bed97195a607a00c806
Unknown <thedragon_master@hotmail.ca>
2019-01-26 14:58:06 -0500

    Counter added to cool down square

--- Online Grid Arena/Assets/Scripts/HUD/CooldownSquare.cs
+++ Online Grid Arena/Assets/Scripts/HUD/CooldownSquare.cs
@@ -6,24 +6,28 @@ public class CooldownSquare : HideableUI
 {
     [SerializeField]
     public Color SquareColor {protected get; set;}
+    public Text SquareText { protected get; set; }
 
     void Start()
     {
         Hide();
         SquareColor = GetComponent<Image>().color;
+        SquareText = GetComponentInChildren<Text>();
     }
 
     public void UpdateSquare(int cooldown, int cooldownRemaining)
     {
+        Text text = SquareText;
         Color color = SquareColor;
         int alpha = 0;
         if (cooldown != 0)
         {
             alpha = (cooldownRemaining / cooldown);
+            text.text = cooldownRemaining.ToString();
         }
         color.a = Convert.ToByte(alpha);
         SquareColor = color;
-        Show();
-         
+        SquareText = text;
+        Show(); 
     }
 }
