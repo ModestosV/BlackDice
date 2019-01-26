7d490d9d79104be9f689e8db5690cd7b82d45195
Unknown <thedragon_master@hotmail.ca>
2019-01-26 16:41:20 -0500

    Changed the cooldown square for comments

--- Online Grid Arena/Assets/Scripts/HUD/CooldownSquare.cs
+++ Online Grid Arena/Assets/Scripts/HUD/CooldownSquare.cs
@@ -5,29 +5,20 @@ using System;
 public class CooldownSquare : HideableUI
 {
     [SerializeField]
-    public Color SquareColor {protected get; set;}
     public Text SquareText { protected get; set; }
 
     void Start()
     {
         Hide();
-        SquareColor = GetComponent<Image>().color;
         SquareText = GetComponentInChildren<Text>();
     }
 
     public void UpdateSquare(int cooldown, int cooldownRemaining)
     {
-        Text text = SquareText;
-        Color color = SquareColor;
-        int alpha = 0;
         if (cooldown != 0)
         {
-            alpha = (cooldownRemaining / cooldown);
-            text.text = cooldownRemaining.ToString();
+            SquareText.text = cooldownRemaining.ToString();
         }
-        color.a = Convert.ToByte(alpha);
-        SquareColor = color;
-        SquareText = text;
         Show(); 
     }
 }
