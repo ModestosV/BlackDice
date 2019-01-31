a4ba97b4c48392139e0ef09493b7889c0a20a1ee
lambhunter0 <george.theophanous@hotmail.com>
2019-01-31 16:55:26 -0500

    #133 moved a few trees and files around, added an animation for activeCircle

--- Online Grid Arena/Assets/Scripts/HUD/ActiveCircle.cs
+++ Online Grid Arena/Assets/Scripts/HUD/ActiveCircle.cs
@@ -4,35 +4,8 @@ public class ActiveCircle : HideableUI
 {
     public bool isActive;
 
-    private SpriteRenderer spriteRenderer;
-    private float flickerOffsetTime = 1.0f;
-    private float currentTime = 1.0f;
-
-    void Awake()
-    {
-        isActive = false;
-        spriteRenderer = GetComponent<SpriteRenderer>();    
-    }
-
     private void Update()
     {
-        currentTime -= Time.deltaTime;
-        if (isActive == false)
-        {
-            spriteRenderer.enabled = false;
-        }
-        else
-        {
-            if (currentTime <= 0.0f)
-            {
-                Flicker();
-                currentTime = flickerOffsetTime;
-            }
-        }
-    }
-
-    private void Flicker()
-    {
-        spriteRenderer.enabled = !spriteRenderer.enabled;
+        enabled = isActive;
     }
 }
