247a87b2d5fef8cad432b03822e99558251356a3
Ryan <rswllee@hotmail.com>
2019-02-24 02:09:32 -0500

    #112 Added basic script for agent frog and empty spots for each of his abilities

---
+++ Online Grid Arena/Assets/Scripts/Abilities/ConcreteAbilities/Hop.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public sealed class Hop : AbstractTargetedAbility
+{
+    public Hop(ICharacter activeCharacter) : base(
+        Resources.Load<Sprite>("Sprites/Abilities/PengwinSlap"),
+        Resources.Load<GameObject>("Prefabs/AbilityAnimations/SlapAnimation"),
+        Resources.Load<AudioClip>("Audio/Ability/slap"),
+        activeCharacter,
+        1,
+        1,
+        AbilityType.TARGET_ENEMY,
+        " ")
+    { }
+
+    protected async override void PrimaryAction(List<IHexTileController> targetTiles)
+    {
+
+    }
+}
---
+++ Online Grid Arena/Assets/Scripts/Abilities/ConcreteAbilities/Lick.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public sealed class Lick : AbstractTargetedAbility
+{
+    public Lick(ICharacter activeCharacter) : base(
+        Resources.Load<Sprite>("Sprites/Abilities/PengwinSlap"),
+        Resources.Load<GameObject>("Prefabs/AbilityAnimations/SlapAnimation"),
+        Resources.Load<AudioClip>("Audio/Ability/slap"),
+        activeCharacter,
+        1,
+        2,
+        AbilityType.TARGET_ENEMY,
+        "Lick - Basic Attack \n Agent Frog will lick the opponent, causing their speed to drop by 2 for 1 turn")
+    { }
+
+    protected async override void PrimaryAction(List<IHexTileController> targetTiles)
+    {
+
+    }
+}
