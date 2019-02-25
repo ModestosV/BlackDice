1e2cfe67226f624b9328e12bd6132cd33503b99b
Ryan <rswllee@hotmail.com>
2019-02-25 03:02:52 -0500

    #112 added damage to lick ability

--- Online Grid Arena/Assets/Scripts/Abilities/ConcreteAbilities/Lick.cs
+++ Online Grid Arena/Assets/Scripts/Abilities/ConcreteAbilities/Lick.cs
@@ -17,6 +17,13 @@ public sealed class Lick : AbstractTargetedAbility
 
     protected async override void PrimaryAction(List<IHexTileController> targetTiles)
     {
+        LickAttack(targetTiles);
+    }
 
+    private void LickAttack(List<IHexTileController> targetTiles)
+    {
+        actionHandler.Damage(character.Controller.CharacterStats["attack"].Value, targetTiles[0].OccupantCharacter);
+        PlaySoundEffect();
+        PlayAnimation(targetTiles[0]);
     }
 }
