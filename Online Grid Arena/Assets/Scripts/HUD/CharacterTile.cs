4052dad75f768c5f86be2ac47d88a6727a50bac2
Nicholas <nchiassonpalacios@gmail.com>
2019-02-26 12:22:04 -0500

    #155 Add indicator for character tiles to show which character is active

--- Online Grid Arena/Assets/Scripts/HUD/CharacterTile.cs
+++ Online Grid Arena/Assets/Scripts/HUD/CharacterTile.cs
@@ -5,16 +5,35 @@ public sealed class CharacterTile : BlackDiceMonoBehaviour
 {
     private RawImage characterIcon;
     private Image border;
+    private GameObject activeIndicator;
+    private Animator activeAnimator;
+
+    [SerializeField]
+    public GameObject ActiveIndicator;
 
     private void Awake()
     {
         characterIcon = GetComponentInChildren<RawImage>();
         border = GetComponent<Image>();
+
+        activeIndicator = Instantiate(ActiveIndicator, this.transform) as GameObject;
+        activeIndicator.transform.SetSiblingIndex(0);
+        activeAnimator = activeIndicator.GetComponent<Animator>();
     }
 
     public void Setup(Texture texture, Color32 color32)
     {
         characterIcon.texture = texture;
         border.color = color32;
+    }
+
+    public void ShowActive()
+    {
+        activeAnimator.SetBool("Active", true);
+    }
+
+    public void HideActive()
+    {
+        activeAnimator.SetBool("Active", false);
     }
 }
