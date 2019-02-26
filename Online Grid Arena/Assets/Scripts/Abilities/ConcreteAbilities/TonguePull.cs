63c28011f60f594efb87b30ad57d32d5fa562eae
Ryan <rswllee@hotmail.com>
2019-02-26 03:03:23 -0500

    #112 added check on tongue pull to only damage enemies

--- Online Grid Arena/Assets/Scripts/Abilities/ConcreteAbilities/TonguePull.cs
+++ Online Grid Arena/Assets/Scripts/Abilities/ConcreteAbilities/TonguePull.cs
@@ -11,12 +11,24 @@ public sealed class TonguePull : AbstractTargetedAbility
         activeCharacter,
         3,
         5,
-        AbilityType.TARGET_ENEMY,
+        AbilityType.TARGET_LINE,
         "Tongue Pull - Special Ability \nAgent Frog pulls an opponent or ally towards himself")
     { }
 
     protected async override void PrimaryAction(List<IHexTileController> targetTiles)
     {
+        var target = targetTiles[0].OccupantCharacter;
 
+        
+        PlaySoundEffect();
+        PlayAnimation(targetTiles[0]);
+    }
+
+    protected override void SecondaryAction(List<IHexTileController> targetTiles)
+    {
+        if (!targetTiles[1].OccupantCharacter.IsAlly(character.Controller))
+        {
+            actionHandler.Damage(character.Controller.CharacterStats["attack"].Value, targetTiles[0].OccupantCharacter);
+        }
     }
 }
