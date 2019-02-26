672c7314d5feaedd854ef3a09fd46ae9a19294bc
lambhunter0 <george.theophanous@hotmail.com>
2019-02-26 13:03:43 -0500

    #163 fields made to lower case

--- Online Grid Arena/Assets/Scripts/HUD/HUDController.cs
+++ Online Grid Arena/Assets/Scripts/HUD/HUDController.cs
@@ -3,59 +3,59 @@ using UnityEngine;
 
 public sealed class HUDController : IHUDController, IEventSubscriber
 {
-    private IStatPanelController SelectedStatPanel;
-    private IPlayerPanel SelectedPlayerPanel;
+    private IStatPanelController selectedStatPanel;
+    private IPlayerPanel selectedPlayerPanel;
 
-    private IStatPanelController TargetStatPanel;
-    private IPlayerPanel TargetPlayerPanel;
+    private IStatPanelController targetStatPanel;
+    private IPlayerPanel targetPlayerPanel;
 
-    private IAbilityPanelController AbilityPanelController;
+    private IAbilityPanelController abilityPanelController;
 
-    private EndTurnButton EndTurnButton;
+    private EndTurnButton endTurnButton;
 
     public HUDController(IStatPanelController selectedStatPanel, IPlayerPanel selectedPlayerPanel, IStatPanelController targetStatPanel, IPlayerPanel targetPlayerPanel, IAbilityPanelController abilityPanelController, EndTurnButton endTurnButton)
     {
-        SelectedStatPanel = selectedStatPanel;
-        SelectedPlayerPanel = selectedPlayerPanel;
-        TargetStatPanel = targetStatPanel;
-        TargetPlayerPanel = targetPlayerPanel;
-        AbilityPanelController = abilityPanelController;
-        EndTurnButton = endTurnButton;
+        this.selectedStatPanel = selectedStatPanel;
+        this.selectedPlayerPanel = selectedPlayerPanel;
+        this.targetStatPanel = targetStatPanel;
+        this.targetPlayerPanel = targetPlayerPanel;
+        this.abilityPanelController = abilityPanelController;
+        this.endTurnButton = endTurnButton;
     }
 
     public void ClearSelectedHUD()
     {
-        SelectedStatPanel.DisableStatDisplays();
-        SelectedPlayerPanel.ClearPlayerName();
-        AbilityPanelController.Hide();
+        selectedStatPanel.DisableStatDisplays();
+        selectedPlayerPanel.ClearPlayerName();
+        abilityPanelController.Hide();
     }
 
     public void UpdateSelectedHUD(Dictionary<string, ICharacterStat> characterStats, string playerName, List<IAbility> abilities, List<IEffect> effects)
     {
-        SelectedStatPanel.EnableStatDisplays();
-        SelectedStatPanel.CharacterStats = characterStats;
-        SelectedStatPanel.UpdateStatValues();
-        SelectedPlayerPanel.SetPlayerName($"Player {playerName}");
-        AbilityPanelController.UpdateAbilityPanel(abilities, effects);
+        selectedStatPanel.EnableStatDisplays();
+        selectedStatPanel.CharacterStats = characterStats;
+        selectedStatPanel.UpdateStatValues();
+        selectedPlayerPanel.SetPlayerName($"Player {playerName}");
+        abilityPanelController.UpdateAbilityPanel(abilities, effects);
     }
 
     public void ClearTargetHUD()
     {
-        TargetStatPanel.DisableStatDisplays();
-        TargetPlayerPanel.ClearPlayerName();
+        targetStatPanel.DisableStatDisplays();
+        targetPlayerPanel.ClearPlayerName();
     }
 
     public void UpdateTargetHUD(Dictionary<string, ICharacterStat> characterStats, string playerName)
     {
-        TargetStatPanel.EnableStatDisplays();
-        TargetStatPanel.CharacterStats = characterStats;
-        TargetStatPanel.UpdateStatValues();
-        TargetPlayerPanel.SetPlayerName($"Player {playerName}");
+        targetStatPanel.EnableStatDisplays();
+        targetStatPanel.CharacterStats = characterStats;
+        targetStatPanel.UpdateStatValues();
+        targetPlayerPanel.SetPlayerName($"Player {playerName}");
     }
 
     public void PulseEndTurnButton()
     {
-        EndTurnButton.Animator.SetBool("isPulsing", true);
+        endTurnButton.Animator.SetBool("isPulsing", true);
         Debug.Log("End Turn Button Animation Pulse start.");
     }
 
@@ -64,7 +64,7 @@ public sealed class HUDController : IHUDController, IEventSubscriber
         var type = @event.GetType();
         if (type == typeof(StartNewTurnEvent))
         {
-            EndTurnButton.Animator.SetBool("isPulsing", false);
+            endTurnButton.Animator.SetBool("isPulsing", false);
             Debug.Log("End Turn Button Animation Pulse stop.");
         }
     }
