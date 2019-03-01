d180789eaa565f1cefb122d8e36fbc423f392e5d
todd829 <patodd829@gmail.com>
2019-03-01 17:53:32 -0500

    #152 Make a stage 1 scene with a complete stage 1 button and a coming soon stage 2 scene

---
+++ Online Grid Arena/Assets/Scripts/Tutorial/Stage1.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Stage1 : HideableUI
+{
+    void Start()
+    {
+        Show();
+    }
+
+    public void CompleteStage1()
+    {
+        EventBus.Publish<StageCompletedEvent>(new StageCompletedEvent(1));
+
+        ExitToMainMenu();
+    }
+
+    public void ExitToMainMenu()
+    {
+
+        AudioSource music = FindObjectOfType<AudioSource>();
+        if (music != null)
+            Destroy(music);
+
+        SceneManager.LoadScene(0);
+    }
+}
---
+++ Online Grid Arena/Assets/Scripts/Tutorial/Stage2.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Stage2 : HideableUI
+{
+    void Start()
+    {
+        Show();
+    }
+
+    public void ExitToMainMenu()
+    {
+
+        AudioSource music = FindObjectOfType<AudioSource>();
+        if (music != null)
+            Destroy(music);
+
+        SceneManager.LoadScene(0);
+    }
+}
