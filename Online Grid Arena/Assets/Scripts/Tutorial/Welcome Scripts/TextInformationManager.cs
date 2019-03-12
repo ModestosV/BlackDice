62d076958ed15d9d125eacadff6af9d3321d99c7
Ryan <rswllee@hotmail.com>
2019-03-12 04:24:33 -0400

    #151 Added some scripts to scroll through text for information for ease of use

---
+++ Online Grid Arena/Assets/Scripts/Tutorial/Welcome Scripts/TextInformationManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextInformationManager : MonoBehaviour
+{
+    private List<string> infoSegment;
+    private int nextInfo;
+
+    void Start()
+    {
+        infoSegment = new List<string>();
+        nextInfo = 0;
+        //StartInfoSegment();
+    }
+
+    public void StartInfoSegment(TextInformation textInfo)
+    {
+        Debug.Log("Information accessed");
+
+        infoSegment.Clear();
+
+        foreach (string info in textInfo.infoBlobs)
+        {
+            infoSegment.Add(info);
+        }
+
+        DisplayNextInfoBlob();
+    }
+
+    public void DisplayNextInfoBlob()
+    {
+        if (nextInfo == infoSegment.Count - 1)
+        {
+            nextInfo = 0;
+        }
+
+        string info = infoSegment[nextInfo];
+        nextInfo++;
+    }
+
+    public void DisplayPrevInfoBlob()
+    {
+        if (nextInfo == 0)
+        {
+            nextInfo = infoSegment.Count - 1;
+        }
+
+        string info = infoSegment[nextInfo];
+        nextInfo--;
+    }
+
+}
