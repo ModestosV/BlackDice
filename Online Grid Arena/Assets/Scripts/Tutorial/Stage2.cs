9fbcc675bf9f4031a9bee2bb978b03b4650d7739
todd829 <patodd829@gmail.com>
2019-03-13 19:22:23 -0400

    #152 add stage 2 controller

--- Online Grid Arena/Assets/Scripts/Tutorial/Stage2.cs
+++ Online Grid Arena/Assets/Scripts/Tutorial/Stage2.cs
@@ -8,6 +8,13 @@ public class Stage2 : HideableUI
         Show();
     }
 
+    public void CompleteStage2()
+    {
+        EventBus.Publish<StageCompletedEvent>(new StageCompletedEvent(2));
+
+        ExitToMainMenu();
+    }
+
     public void ExitToMainMenu()
     {
         SceneManager.LoadScene(2);
---
+++ Online Grid Arena/Assets/Scripts/Tutorial/Stage2Controller.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine.SceneManagement;
+
+public class Stage2Controller: IEventSubscriber
+{
+    private ICharacterController character;
+
+    public Stage2Controller(ICharacterController character)
+    {
+        this.character = character;
+
+        EventBus.Subscribe<StartNewTurnEvent>(this);
+    }
+
+    public void Handle(IEvent @event)
+    {
+        var type = @event.GetType();
+
+        if (type == typeof(StartNewTurnEvent))
+        {
+            EventBus.Publish(new StageCompletedEvent(2));
+            SceneManager.LoadScene(2);
+        }
+    }
+}
