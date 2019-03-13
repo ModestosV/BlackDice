f7600b5400d701bd66c0f1d011927849d2c3e040
todd829 <patodd829@gmail.com>
2019-03-13 19:22:23 -0400

    #152 add stage 2 controller

--- Online Grid Arena/Assets/Scripts/Tutorial/TutorialGameManager.cs
+++ Online Grid Arena/Assets/Scripts/Tutorial/TutorialGameManager.cs
@@ -7,7 +7,7 @@ public sealed class TutorialGameManager : MonoBehaviour
 {
     [SerializeField] private int tutorialStageIndex;
 
-    private List<Action> tutorialStageAwakeMethods = new List<Action>();
+    private List<Action> tutorialStageStartMethods = new List<Action>();
 
     private TurnController turnController;
     private HUDController hudController;
@@ -35,20 +35,101 @@ public sealed class TutorialGameManager : MonoBehaviour
 
         EventBus.Reset();
 
-        tutorialStageAwakeMethods.Add(() => this.AwakeStageMovement());
-        tutorialStageAwakeMethods.Add(() => this.AwakeStageAttack());
-
-        tutorialStageAwakeMethods[this.tutorialStageIndex].Invoke();
-
         Debug.Log(ToString() + " Awake() end");
     }
 
-    private void AwakeStageMovement()
+    private void StartStageMovement()
     {
+        // Get all characters from scene
+        characterControllers = FindObjectsOfType<AbstractCharacter>().Select(x => x.Controller).ToList();
 
+        //Initialize players
+        players = new List<IPlayer>() { new Player("1") };
+        players[0].AddCharacterController(characterControllers[0]);
+
+        //Initialize character panels
+        characterPanels = FindObjectsOfType<CharacterPanel>().ToList();
+
+        Debug.Log("Character Panels size: " + characterPanels.Count);
+        Debug.Log("Character tiles size: " + characterPanels[0].CharacterTiles.Length);
+
+        characterPanels[0].CharacterTiles[0].Setup(players[0].CharacterControllers[0]);
+
+        // Initialize turn controller
+        turnController = new TurnController(players);
+
+        // Initialize Menus
+        endMatchMenu = FindObjectOfType<EndMatchMenu>();
+        matchMenu = FindObjectOfType<MatchMenu>();
+
+        // Initialize HUD
+        StatPanel[] statPanels = FindObjectsOfType<StatPanel>();
+        PlayerPanel[] playerPanels = FindObjectsOfType<PlayerPanel>();
+        AbilityPanel abilityPanel = FindObjectOfType<AbilityPanel>();
+        AbilityPanelController abilityPanelController = new AbilityPanelController(abilityPanel);
+
+        hudController = new HUDController(statPanels[1].Controller, playerPanels[0], statPanels[0].Controller, playerPanels[1], abilityPanelController, FindObjectOfType<EndTurnButton>());
+
+        // Initialize selection controllers
+        gridSelectionController = new GridSelectionController();
+        freeSelectionController = new FreeSelectionController(gridSelectionController);
+        movementSelectionController = new MovementSelectionController(gridSelectionController);
+        targetEnemyAbilitySelectionController = new TargetEnemyAbilitySelectionController(gridSelectionController);
+        targetAllyAbilitySelectionController = new TargetAllyAbilitySelectionController(gridSelectionController);
+        targetTileAbilitySelectionController = new TargetTileAbilitySelectionController(gridSelectionController);
+        targetLineAbilitySelectionController = new TargetLineAbilitySelectionController(gridSelectionController);
+        targetLineAOEAbilitySelectionController = new TargetLineAOEAbilitySelectionController(gridSelectionController);
+
+        var selectionControllers = new Dictionary<string, ISelectionController>()
+        {
+            { "free", freeSelectionController },
+            { "movement", movementSelectionController },
+            { "target_enemy", targetEnemyAbilitySelectionController },
+            { "target_ally", targetAllyAbilitySelectionController },
+            { "target_tile", targetTileAbilitySelectionController },
+            { "target_line", targetLineAbilitySelectionController },
+            { "target_line_aoe", targetLineAOEAbilitySelectionController }
+        };
+
+        selectionManager = new SelectionManager(turnController, gridSelectionController, selectionControllers);
+
+        // Initialize input manager
+        inputManager = FindObjectOfType<InputManager>();
+        inputManager.SelectionManager = selectionManager;
+
+        // Initialize characters
+        List<ICharacterController> characters = FindObjectsOfType<AbstractCharacter>().Select(x => x.Controller).ToList();
+        foreach (ICharacterController character in characters)
+        {
+            character.HUDController = hudController;
+        }
+
+        // Initialize Event Subscribing
+        //EventBus.Subscribe<StartNewTurnEvent>(turnController);
+        EventBus.Subscribe<UpdateSelectionModeEvent>(selectionManager);
+        EventBus.Subscribe<DeselectSelectedTileEvent>(gridSelectionController);
+        EventBus.Subscribe<SelectTileEvent>(gridSelectionController);
+        EventBus.Subscribe<SelectActivePlayerEvent>(turnController);
+        EventBus.Subscribe<SelectTileEvent>(turnController);
+        EventBus.Subscribe<StartNewTurnEvent>(hudController);
+
+        foreach (CharacterTile tile in FindObjectsOfType(typeof(CharacterTile)))
+        {
+            EventBus.Subscribe<ActiveCharacterEvent>(tile);
+            EventBus.Subscribe<ExhaustCharacterEvent>(tile);
+            EventBus.Subscribe<NewRoundEvent>(tile);
+            EventBus.Subscribe<StatusEffectEvent>(tile);
+        }
+
+        // Start Game
+        FindObjectOfType<Grid>().InitializeGrid(gridSelectionController);
+        EventBus.Publish(new StartNewTurnEvent());
+        EventBus.Publish(new UpdateSelectionModeEvent(SelectionMode.FREE));
+
+        Stage2Controller stage2Controller = new Stage2Controller(characterControllers[0]);
     }
 
-    private void AwakeStageAttack()
+    private void StartStageAttack()
     {
         // Marc's Tutorial Stage
     }
@@ -57,8 +138,10 @@ public sealed class TutorialGameManager : MonoBehaviour
     {
         Debug.Log(ToString() + " Start() begin");
 
-        FindObjectOfType<Grid>().InitializeGrid(gridSelectionController);
-        EventBus.Publish(new StartNewTurnEvent());
+        tutorialStageStartMethods.Add(() => this.StartStageMovement());
+        tutorialStageStartMethods.Add(() => this.StartStageAttack());
+
+        tutorialStageStartMethods[this.tutorialStageIndex].Invoke();
 
         Debug.Log(ToString() + " Start() end");
     }
