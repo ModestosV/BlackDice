efe652a16fcbcbd40b0f231275583e147586784d
ModestosV <modestosvergados@gmail.com>
2019-03-13 18:15:14 -0400

    #208 Clicking on a selected active character now puts them in movement mode

--- Online Grid Arena/Assets/Scripts/Character/Interfaces/ICharacterController.cs
+++ Online Grid Arena/Assets/Scripts/Character/Interfaces/ICharacterController.cs
@@ -19,6 +19,8 @@ public interface ICharacterController
     SpriteRenderer ActiveCircle { get; set; }
     MeshRenderer Shield { set; }
     bool IsShielded { get; set; }
+    bool IsActive { get; }
+
     void ExecuteAbility(int abilityNumber, List<IHexTileController> targetTiles);
     void ExecuteMove(List<IHexTileController> path);
     void Refresh();
