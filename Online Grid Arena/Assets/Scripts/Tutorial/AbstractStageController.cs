9a31d661025b36fd661fb1d780ba907d06475a3f
todd829 <patodd829@gmail.com>
2019-03-15 23:27:17 -0400

    #145 #152 replace IStageController with an AbstractStageController

---
+++ Online Grid Arena/Assets/Scripts/Tutorial/AbstractStageController.cs
@@ -0,0 +1,8 @@
+﻿
+public class AbstractStageController
+{
+    public void CompleteStage(int stageIndex)
+    {
+        EventBus.Publish(new StageCompletedEvent(stageIndex));
+    }
+}
--- Online Grid Arena/Assets/Scripts/Tutorial/IStageController.cs
+++
@@ -1,5 +0,0 @@
-﻿
-public interface IStageController
-{
-    void CompleteStage();
-}
