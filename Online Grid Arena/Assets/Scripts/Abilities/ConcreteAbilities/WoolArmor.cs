38e457d8b2dca5b65e4f50b4a388cf6a4081ddc8
lambhunter0 <george.theophanous@hotmail.com>
2019-03-16 01:08:15 -0400

    #115 added sheep character with passive E

---
+++ Online Grid Arena/Assets/Scripts/Abilities/ConcreteAbilities/WoolArmor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public sealed class WoolArmor : AbstractPassiveAbility
+{
+    public WoolArmor(ICharacter activeCharacter, IEffect effect) : base(
+        Resources.Load<Sprite>("Sprites/Abilities/poison"),
+        activeCharacter,
+        "Wool Armor - Passive Ability \nAt the end of his turn, Sheepadin's armor grows thicker. Gain a stack of Wool Armor.",
+        true)
+    {
+        this.AddEffect(effect);
+    }
+
+    protected override void PrimaryAction(List<IHexTileController> targetTile)
+    {
+        this.character.Controller.ApplyEffect(this.Effects[0]);
+    }
+}
---
+++ Online Grid Arena/Assets/Scripts/Character/Sheepadin.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public sealed class Sheepadin : AbstractCharacter
+{
+    protected override void Awake()
+    {
+        base.Awake();
+        IAbility placeholder = new Placeholder(this);
+        IEffect woolArmorEffect = new WoolArmorEffect();
+        IAbility woolArmor = new WoolArmor(this, woolArmorEffect);
+
+        var abilities = new List<IAbility>() { placeholder, placeholder, woolArmor, placeholder };
+        var effects = new List<IEffect>() { };
+
+        var characterStats = InitializeStats(120, 5, 20, 100);
+
+        characterController = new CharacterController(this)
+        {
+            Owner = playerName,
+            CharacterIcon = characterIcon,
+            BorderColor = borderColor,
+            HealthBar = healthBar.GetComponent<HealthBar>(),
+            Abilities = abilities,
+            CharacterStats = characterStats,
+            Effects = effects,
+            Shield = shield.GetComponent<MeshRenderer>()
+        };
+    }
+}
