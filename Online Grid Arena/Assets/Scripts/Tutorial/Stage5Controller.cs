249e221a6e3260f69f02d66a84d0fb0ab8578d37
todd829 <patodd829@gmail.com>
2019-03-18 17:46:36 -0400

    #197 handle first 3 steps

--- Online Grid Arena/Assets/Scripts/Tutorial/Stage5Controller.cs
+++ Online Grid Arena/Assets/Scripts/Tutorial/Stage5Controller.cs
@@ -6,17 +6,18 @@ using System.Collections.Generic;
 public class Stage5Controller : AbstractStageController, IEventSubscriber
 {
     private const String TUTORIAL_STEP_1 = "Click on Rocket Cat";
-    private const String TUTORIAL_STEP_2 = "Press Q\nattack Pengwin";
-    private const String TUTORIAL_STEP_3 = "Check Buff\nPress End Turn";
-    private const String TUTORIAL_STEP_4 = "Click on Pengwin";
-    private const String TUTORIAL_STEP_5 = "Press E to Buff\nNearby Ally";
-    private const String TUTORIAL_STEP_6 = "Check Buff\nClick on DefaultCharacter";
-    private const String TUTORIAL_STEP_7 = "Check Buff\nPress End Turn";
-    private const String TUTORIAL_STEP_8 = "Click on Pengwin";
-    private const String TUTORIAL_STEP_9 = "Press R\nAttack Rocket Cat";
-    private const String TUTORIAL_STEP_10 = "End Turn";
-    private const String TUTORIAL_STEP_11 = "Click on Rocket Cat";
-    private const String TUTORIAL_STEP_12 = "Check Buff stack 4\nEnd Turn";
+    private const String TUTORIAL_STEP_2 = "Press Q";
+    private const String TUTORIAL_STEP_3 = "Attack Pengwin";
+    private const String TUTORIAL_STEP_4 = "Check Buff\nPress End Turn";
+    private const String TUTORIAL_STEP_5 = "Click on Pengwin";
+    private const String TUTORIAL_STEP_6 = "Press E to Buff\nNearby Ally";
+    private const String TUTORIAL_STEP_7 = "Check Buff\nClick on DefaultCharacter";
+    private const String TUTORIAL_STEP_8 = "Check Buff\nPress End Turn";
+    private const String TUTORIAL_STEP_9 = "Click on Pengwin";
+    private const String TUTORIAL_STEP_10 = "Press R\nAttack Rocket Cat";
+    private const String TUTORIAL_STEP_11 = "End Turn";
+    private const String TUTORIAL_STEP_12 = "Click on Rocket Cat";
+    private const String TUTORIAL_STEP_13 = "Check Buff stack 4\nEnd Turn";
     private const String STAGE_COMPLETE = "Stage Completed!\nRedirecting Tutorial";
     private const int STAGE_INDEX = 5;
 
@@ -28,6 +29,8 @@ public class Stage5Controller : AbstractStageController, IEventSubscriber
 
     private int currentStepIndex = 0;
 
+    private int abilityIndexSelected = -1;
+    private IHexTileController selectedTile = null;
     private SelectionMode selectionMode = SelectionMode.FREE;
 
     public Stage5Controller(ICharacterController rocketCat, ICharacterController pengwin, ICharacterController defaultCharacter)
@@ -35,6 +38,10 @@ public class Stage5Controller : AbstractStageController, IEventSubscriber
         this.rocketCat = rocketCat;
         this.pengwin = pengwin;
         this.defaultCharacter = defaultCharacter;
+
+        rocketCat.CharacterStats["moves"].BaseValue = 0;
+        pengwin.CharacterStats["moves"].BaseValue = 0;
+        defaultCharacter.CharacterStats["moves"].BaseValue = 0;
 
         stepMethods.Add(() => this.handleStep1());
         stepMethods.Add(() => this.handleStep2());
@@ -53,17 +60,38 @@ public class Stage5Controller : AbstractStageController, IEventSubscriber
 
     private void handleStep1()
     {
-
+        if (rocketCat.IsActive)
+        {
+            GameObject.FindWithTag("TutorialTooltip").GetComponent<TextMeshProUGUI>().text = TUTORIAL_STEP_2;
+            currentStepIndex += 1;
+        }
     }
 
     private void handleStep2()
     {
-
+        if (rocketCat.IsActive)
+        {
+            if (abilityIndexSelected == 0)
+            {
+                GameObject.FindWithTag("TutorialTooltip").GetComponent<TextMeshProUGUI>().text = TUTORIAL_STEP_3;
+                currentStepIndex += 1;
+            }
+        }
+        else
+        {
+            GameObject.FindWithTag("TutorialTooltip").GetComponent<TextMeshProUGUI>().text = TUTORIAL_STEP_1;
+            currentStepIndex = 0;
+        }
     }
 
     private void handleStep3()
     {
-
+        if (pengwin.CharacterStats["health"].CurrentValue < pengwin.CharacterStats["health"].BaseValue && selectionMode == SelectionMode.FREE)
+        {
+            GameObject.FindWithTag("TutorialTooltip").GetComponent<TextMeshProUGUI>().text = TUTORIAL_STEP_4;
+            currentStepIndex += 1;
+            selectedTile = null;
+        }
     }
 
     private void handleStep4()
@@ -113,6 +141,22 @@ public class Stage5Controller : AbstractStageController, IEventSubscriber
 
     public void Handle(IEvent @event)
     {
+        var type = @event.GetType();
 
+        if (type == typeof(AbilitySelectedEvent))
+        {
+            var abilitySelectedEvent = (AbilitySelectedEvent)@event;
+
+            abilityIndexSelected = abilitySelectedEvent.AbilityIndex;
+        }
+
+        if (type == typeof(UpdateSelectionModeEvent))
+        {
+            var selectionModeEvent = (UpdateSelectionModeEvent)@event;
+
+            selectionMode = selectionModeEvent.SelectionMode;
+        }
+
+        stepMethods[currentStepIndex].Invoke();
     }
 }
