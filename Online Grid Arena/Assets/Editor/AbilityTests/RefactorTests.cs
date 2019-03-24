a426f524f4bdf5fd7bfacc388fe55a0396612b81
ModestosV <modestosvergados@gmail.com>
2019-03-24 17:56:05 -0400

    #148 Add Refactor unit test

---
+++ Online Grid Arena/Assets/Editor/AbilityTests/RefactorTests.cs
@@ -0,0 +1,36 @@
+﻿using NSubstitute;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+public class RefactorTests
+{
+    Refactor sut;
+
+    ICharacter character;
+    ICharacterController characterController;
+    ICharacterController enemyCharacterController;
+    IHexTileController hexTileController;
+
+    [SetUp]
+    public void Init()
+    {
+        character = Substitute.For<ICharacter>();
+        characterController = Substitute.For<ICharacterController>();
+        enemyCharacterController = Substitute.For<ICharacterController>();
+        hexTileController = Substitute.For<IHexTileController>();
+
+        character.Controller.Returns(characterController);
+
+        sut = new Refactor(character);
+    }
+
+    [Test]
+    public void Stuns_enemy()
+    {
+        hexTileController.OccupantCharacter.Returns(enemyCharacterController);
+
+        sut.Execute(new List<IHexTileController>() { hexTileController });
+
+        enemyCharacterController.Received(1).StatusEffectState = StatusEffectState.STUNNED;
+    }
+}
