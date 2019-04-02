5fc8806e4cff20078a25ec028299cf247e3eb29b
todd829 <patodd829@gmail.com>
2019-04-02 12:07:01 -0400

    #196 add some tutorial text for steps and change some String to string

--- Online Grid Arena/Assets/Scripts/Tutorial/Stage4Controller.cs
+++ Online Grid Arena/Assets/Scripts/Tutorial/Stage4Controller.cs
@@ -2,6 +2,10 @@
 
 public class Stage4Controller : AbstractStageController, IEventSubscriber
 {
+    private const string TUTORIAL_STEP_1 = "Click On Sheepadin";
+    private const string TUTORIAL_STEP_2 = "Press W";
+    private const string TUTORIAL_STEP_3 = "Heal Both characters";
+
     private ICharacterController rocketCat;
     private ICharacterController sheepadin;
     private SelectionMode selectionMode = SelectionMode.FREE;
