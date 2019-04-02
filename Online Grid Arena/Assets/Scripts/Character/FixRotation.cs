ba8230fd04cb89370168dbe32f0f0ea75f96941f
Ryan <rswllee@hotmail.com>
2019-04-02 02:18:46 -0400

    #234 added script to fix child rotation

---
+++ Online Grid Arena/Assets/Scripts/Character/FixRotation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixRotation : MonoBehaviour
+{
+    Quaternion rotation;
+
+    void Awake()
+    {
+        rotation = transform.rotation;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.rotation = rotation;
+    }
+}
