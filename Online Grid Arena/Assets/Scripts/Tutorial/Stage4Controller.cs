1aaf80fa00e46c4ba9b3634fb752c6c3dd1a80ac
todd829 <patodd829@gmail.com>
2019-04-02 12:08:46 -0400

    #196 add stepMethods Action list in controller

--- Online Grid Arena/Assets/Scripts/Tutorial/Stage4Controller.cs
+++ Online Grid Arena/Assets/Scripts/Tutorial/Stage4Controller.cs
@@ -1,10 +1,14 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
 
 public class Stage4Controller : AbstractStageController, IEventSubscriber
 {
     private const string TUTORIAL_STEP_1 = "Click On Sheepadin";
     private const string TUTORIAL_STEP_2 = "Press W";
     private const string TUTORIAL_STEP_3 = "Heal Both characters";
+
+    private List<Action> stepMethods = new List<Action>();
+    private int currentStepIndex = 0;
 
     private ICharacterController rocketCat;
     private ICharacterController sheepadin;
