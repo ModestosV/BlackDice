bb85b603bf0a1d9116c8ab70758e7eee9ac0d7fa
Mathieu Leblanc <0mat.leb2@gmail.com>
2019-04-01 21:52:22 -0400

    Temporarily substitute tutorial 5 for tutorial 4 for poster day

--- Online Grid Arena/Assets/Scripts/Tutorial/TutorialMenu.cs
+++ Online Grid Arena/Assets/Scripts/Tutorial/TutorialMenu.cs
@@ -7,7 +7,7 @@ using UnityEngine;
 
 public class TutorialMenu: HideableUI, IEventSubscriber
 {
-    private const int MAX_STAGE_ALLOWED = 2;
+    private const int MAX_STAGE_ALLOWED = 4;
 
     private string filepath = "";
     private int stagesCompleted = 0;
