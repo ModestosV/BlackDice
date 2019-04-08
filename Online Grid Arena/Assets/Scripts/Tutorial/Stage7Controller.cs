f3189b1b3dadd26bcb1d7ed6860867860e1cc9db
todd829 <patodd829@gmail.com>
2019-04-08 13:37:45 -0400

    #203 create stage 7 controller

---
+++ Online Grid Arena/Assets/Scripts/Tutorial/Stage7Controller.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Stage7Controller : AbstractStageController, IEventSubscriber
+{
+    private const string STAGE_FAILED = "Stage Failed!\nYou lost!\nRedirecting Tutorial";
+    private const int STAGE_INDEX = 7;
+
+    private GridSelectionController gridSelectionController;
+
+    public Stage7Controller(GridSelectionController gridSelectionController)
+    {
+        this.gridSelectionController = gridSelectionController;
+    }
+
+    private void StageFailed()
+    {
+        GameObject.FindWithTag("TutorialTooltip").GetComponent<TextMeshProUGUI>().text = STAGE_FAILED;
+        EventBus.Publish(new SurrenderEvent());
+    }
+
+    public void Handle(IEvent @event)
+    {
+        var type = @event.GetType();
+    }
+}
